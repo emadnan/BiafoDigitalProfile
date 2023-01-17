a3b0b4f9d4e644716296dd085a8567def1e09926
RafayDev <imabdulrafayy@gmail.com>
2023-01-17 17:29:08 +0500

    featues in landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -125,17 +125,78 @@ html {
         </section>
     </header>
     <main>
-    <!-- <svg viewBox="0 0 1440 319">
-        <path fill="#ad021c" fill-opacity="1" d="M0,32L48,80C96,128,192,224,288,224C384,224,480,128,576,90.7C672,53,768,75,864,96C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-      </svg> -->
+        <!-- FEATURES -->
+        <section id="feature">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 mt-2"></div>
+                    <div class="col-md-4 mt-5">
+                        <!-- <div style="background-color:#ff9d00"> -->
+                        <h1 class="text-center mt-3 rounded-pill px-4 py-2"
+                            style="background-color:#ff9d00;">Features
+                        </h1>
+
+                        <!-- </div> -->
+                    </div>
+                    <div class="col-md-4"></div>
+                </div>
+                <div class="row mt-5">
+                    <div class="col-md-3">
+                        <div class="card" style="background-color: #ad021c; border-radius: 50px;">
+                            <div class="card-body">
+                            <h1 class="text-center"  style="background-color:#ff9d00;border-radius: 100px;"><strong><i class="bi bi-border-all"></i></strong></h1>
+                            <p class="text-center text-white" style="font-size:18px;"> Lorem ipsum dolor sit amet, qui suavitate sententiae an.
+                                Ius unum primis oportere ex, his
+                                id paulo graece tacimates, Lorem ipsum dolor sit amet, qui suavitate sententiae an. Ius
+                                unum primis oportere ex, his id paulo graece tacimates, Lorem ipsum dolor sit amet, qui</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="background-color: #ad021c; border-radius: 50px;">
+                            <div class="card-body">
+                            <h1 class="text-center"  style="background-color:#ff9d00;border-radius: 100px;"><strong><i class="bi bi-border-all"></i></strong></h1>
+                            <p class="text-center text-white" style="font-size:18px;"> Lorem ipsum dolor sit amet, qui suavitate sententiae an.
+                                Ius unum primis oportere ex, his
+                                id paulo graece tacimates, Lorem ipsum dolor sit amet, qui suavitate sententiae an. Ius
+                                unum primis oportere ex, his id paulo graece tacimates, Lorem ipsum dolor sit amet, qui</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="background-color: #ad021c; border-radius: 50px;">
+                            <div class="card-body">
+                            <h1 class="text-center"  style="background-color:#ff9d00;border-radius: 100px;"><strong><i class="bi bi-border-all"></i></strong></h1>
+                            <p class="text-center text-white" style="font-size:18px;"> Lorem ipsum dolor sit amet, qui suavitate sententiae an.
+                                Ius unum primis oportere ex, his
+                                id paulo graece tacimates, Lorem ipsum dolor sit amet, qui suavitate sententiae an. Ius
+                                unum primis oportere ex, his id paulo graece tacimates, Lorem ipsum dolor sit amet, qui</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="card" style="background-color: #ad021c; border-radius: 50px;">
+                            <div class="card-body">
+                            <h1 class="text-center"  style="background-color:#ff9d00;border-radius: 100px;"><strong><i class="bi bi-border-all"></i></strong></h1>
+                            <p class="text-center text-white" style="font-size:18px;"> Lorem ipsum dolor sit amet, qui suavitate sententiae an.
+                                Ius unum primis oportere ex, his
+                                id paulo graece tacimates, Lorem ipsum dolor sit amet, qui suavitate sententiae an. Ius
+                                unum primis oportere ex, his id paulo graece tacimates, Lorem ipsum dolor sit amet, qui</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
         <section class="about_curved_bg mt-3" id="about">
             <div class="container">
                 <div class="row">
-                <div class="col-md-4 mt-2"></div>
+                    <div class="col-md-4 mt-2"></div>
                     <div class="col-md-4 mt-5">
-                      <!-- <div style="background-color:#ff9d00"> -->
-                        <h1 class="text-center mt-3 rounded-pill px-4 py-2" style="background-color:#ff9d00">ABOUT US </h1>
-<!-- </div> -->
+                        <!-- <div style="background-color:#ff9d00"> -->
+                        <h1 class="text-center mt-3 rounded-pill px-4 py-2" style="background-color:#ff9d00">ABOUT US
+                        </h1>
+                        <!-- </div> -->
                     </div>
                     <div class="col-md-4"></div>
                 </div>
@@ -143,7 +204,8 @@ html {
                 <div class="row mt-3">
                     <div class="col-md-6 mt-3">
                         <div>
-                            <img src="{{asset('frontend/img/about_3.png')}}" class="mt-5"alt="" width="300" height="400">
+                            <img src="{{asset('frontend/img/about_3.png')}}" class="mt-5" alt="" width="300"
+                                height="400">
                         </div>
                     </div>
                     <div class="col-md-6">
@@ -165,30 +227,32 @@ html {
                                 Ius unum primis oportere ex, his id paulo graece tacimates, Lorem ipsum dolor sit amet,
                                 qui suavitate sententiae an. Ius unum primis oportere ex, his id paulo graece tacimates,
                             </p>
-                           
+
                         </div>
                     </div>
                 </div>
             </div>
         </section>
     </main>
-           
-        <footer>
-            <section class="text-white text-center bg_main_content_yellowish mt-5" style="padding: 15px;">
-              
-              <div class="container">
+
+    <footer>
+        <section class="text-white text-center bg_main_content_yellowish mt-5" style="padding: 15px;">
+
+            <div class="container">
                 <div class="d-flex justify-content-between text-white">
-                  <div class="py-2">
-                    <a href="#contact" class="text-white text-decoration-none">Contact us</a> <span style="color:#304367">|</span> <a
-                      href="/privacy-policy" class="text-white text-decoration-none">Privacy Policy</a>
-                  </div>
-                  <div class="py-2">
-                  <p class="mb-0">Copyright @ 2022. <a href="#" class="text-white">Biafotech.</a> All rights reserved.</p>
-                  </div>
+                    <div class="py-2">
+                        <a href="#contact" class="text-white text-decoration-none">Contact us</a> <span
+                            style="color:#304367">|</span> <a href="/privacy-policy"
+                            class="text-white text-decoration-none">Privacy Policy</a>
+                    </div>
+                    <div class="py-2">
+                        <p class="mb-0">Copyright @ 2022. <a href="#" class="text-white">Biafotech.</a> All rights
+                            reserved.</p>
+                    </div>
                 </div>
-              </div>
-            </section>
-        </footer>
+            </div>
+        </section>
+    </footer>
     <!-- Bootstrap core JS-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Core theme JS-->
