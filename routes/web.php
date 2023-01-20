3f83e15eb9e451d82185d9baa724fab5d53f8d61
MiAwan <malikirfanofficial3332@gmail.com>
2023-01-20 09:58:57 +0500

    create update route

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::get('/pricing', [App\Http\Controllers\PricingController::class, 'pricing'
 Route::post('/add_card',[CardController::class,'addCard'])->name('add_card');
 Route::get('/view_card/{card_id}/{type}', [CardController::class, 'getcard'])->name('view_card');
 Route::get('/delete_card/{id}',[CardController::class, 'delete_card'])->name('view_card');
+Route::get('/delete_card/{id}',[CardController::class, 'delete_card'])->name('delete_card');
+Route::post('/update_card/{id}',[CardController::class, 'update_card'])->name('update_card');
 
