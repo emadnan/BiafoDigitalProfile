89b1b17a8a8e51c4f6e6e1c1642445bac2d4935c
RafayDev <imabdulrafayy@gmail.com>
2023-02-16 15:39:42 +0500

    bug check

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @php
 $permissions= session()->get('permissions');
-$is_new = session()->get('is_new');
+<!-- $is_new = session()->get('is_new'); -->
 @endphp
 
 <style>
@@ -13,7 +13,7 @@ $is_new = session()->get('is_new');
         border-radius: 10px;
     } */
     </style>
-<input type="hidden" id="is_new" value="{{$is_new}}" />
+<!-- <input type="hidden" id="is_new" value="{{$is_new}}" /> -->
 <div class="content-wrapper">
     <div class="container">
         <section class="content-header">
@@ -276,10 +276,10 @@ $(document).ready(function() {
     $('#add_card').click(function() {
         $('#add_card_modal').modal('show');
     });
-    var is_new = $('#is_new').val();
-    if (is_new == '1') {
-        $('#company_profile_modal').modal('show');
-    }
+    // var is_new = $('#is_new').val();
+    // if (is_new == '1') {
+    //     $('#company_profile_modal').modal('show');
+    // }
     var logo=document.getElementById("logo");
     var logo_preview=document.getElementById("logo_preview");
     logo.addEventListener('change',function(){
