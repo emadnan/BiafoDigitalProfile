acd7d3b1cb5d08fd28166ee41677ef02575adb23
RafayDev <imabdulrafayy@gmail.com>
2023-02-16 16:39:16 +0500

    debugging 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,9 +30,12 @@ class HomeController extends Controller
         // print_r("hello");
         // die;
         $user_id=auth()->user()->id;
-        print_r($user_id);
-        die;
+        // print_r($user_id);
+        // die;
         $cards = Card::where('user_id',$user_id)->orwhere('company_user_id',$user_id)->get();
+        echo '<pre>';
+        print_r($cards);
+        die;
         $companies = Company::all();
         $countries = Country::all();
         $cities = City::all();
