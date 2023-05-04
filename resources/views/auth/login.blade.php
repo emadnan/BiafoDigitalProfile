4175b7aef3ab8a71853ec5165ffba4d4bae560b5
Sultan <imsultanzubair@gmail.com>
2023-05-04 11:37:27 +0500

    link add

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Cardify - Login</title>
+    <link rel="icon" href="{{asset('frontend/img/favIcon.png')}}" />
     <script src="{{asset('frontend/js/jquery-3.6.0.min.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"
         integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous">
