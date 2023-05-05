4fc2b6d1b34e68f0c3d061ceb2542c1eb856f37c
Sultan <imsultanzubair@gmail.com>
2023-05-05 16:29:00 +0500

    update faq

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -16,6 +16,7 @@
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="{{ asset('frontend/css/landing_page_styles.css') }}" rel="stylesheet" />
     <link href="{{ asset('frontend\css\bootstrap5.min.css') }}" rel="stylesheet">
+   
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -34,7 +35,7 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation" style="background-color: #e49700;">
-                    <span class="navbar-toggler-icon"><i class="fa-solid fa-bars" style="color: #ffffff;"></i></span>
+                    <span class="navbar-toggler-icon"><i class="fa-solid fa-bars"></i></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
