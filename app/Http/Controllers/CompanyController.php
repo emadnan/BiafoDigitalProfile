6dbef170e21fcd29c70f83c41fd67b0be8ae4537
SultanZubair <sultanzubairhassan@gmail.com>
2023-07-21 11:45:29 +0500

    apply middleware on each route

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,6 +14,21 @@ use Illuminate\Support\Facades\Auth;
 
 class CompanyController extends Controller
 {
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
     public function index()
     {
         $company=Company::find(Auth::user()->company_id);
